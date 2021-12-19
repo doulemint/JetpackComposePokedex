384c4ab72e68efb4aa1d90a4bd5772484bc89f3e
xiya <871251700@qq.com>
2021-12-19 21:22:26 +0800

    finish coding

---
+++ app/src/main/java/com/ysfcyln/jetpackcomposepokedex/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.ysfcyln.jetpackcomposepokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.ysfcyln.jetpackcomposepokedex.ui.screen.pokemondetail.PokemonDetailScreen
+import com.ysfcyln.jetpackcomposepokedex.ui.screen.pokemonlist.PokemonListScreen
+import com.ysfcyln.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposePokedexTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemon_list_screen"
+                ) {
+                    // Create Screens
+                    // Pokemon List Screen
+                    composable("pokemon_list_screen") {
+                        PokemonListScreen(navController = navController)
+                    }
+                    // Pokemon Detail Screen
+                    composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                        arguments = listOf(
+                            navArgument(name = "dominantColor") {
+                                type = NavType.IntType
+                            },
+                            navArgument(name = "pokemonName") {
+                                type = NavType.StringType
+                            }
+                        )) {
+                        // Get arguments
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                        PokemonDetailScreen(
+                            dominantColor = dominantColor,
+                            pokemonName = pokemonName?.toLowerCase(Locale.ROOT) ?: "",
+                            navController = navController
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
